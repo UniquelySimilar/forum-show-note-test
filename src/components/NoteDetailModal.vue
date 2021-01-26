1f186b7dde1fa4907e022f53a18de6c3c9d2b952
UniquelySimilar <consciousexplorer@gmail.com>
2021-01-26 16:24:07 -0700

    initial commit

---
+++ src/components/NoteDetailModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="modal-dialog" class="modal-dialog-container">
+
+    <div class="modal-dialog-content">
+      <div class="modal-dialog-header">
+        <h4>Note Detail</h4>
+      </div>
+      <div class="modal-dialog-body">
+        <p><span class="label">ID:</span>{{ note.id }}</p>
+        <p><span class="label">TITLE:</span>{{ note.title }}</p>
+        <p><span class="label">BODY:</span>{{ note.body }}</p>
+        <p><span class="label">DATE:</span>{{ note.date }}</p>
+      </div>
+      <div class="modal-dialog-footer">
+        <div class="btn-wrapper">
+          <button type="button" class="btn btn-secondary btn-sm close-btn" @click="closeModal">Close</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      note: {
+        type: Object,
+        required: true
+      },
+    },
+    methods: {
+      closeModal() {
+        this.$emit('close-modal-event');
+      },
+    }
+  }
+</script>
+
+<style scoped>
+  .modal-dialog-container {
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%;
+    /* Full width */
+    height: 100%;
+    /* Full height */
+    overflow: auto;
+    /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0);
+    /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4);
+    /* Black w/ opacity */
+  }
+
+  .modal-dialog-content {
+    background-color: #fefefe;
+    margin: 10% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    border-radius: 0.3rem;
+    width: 30%;
+  }
+
+  .modal-dialog-footer {
+    text-align: center;
+  }
+
+  .close-btn {
+    margin-left: 1em;
+  }
+
+  .label {
+    font-weight: bold;
+    margin-right: 1em;
+  }
+</style>
---
+++ src/components/NoteList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <h3>Note List</h3>
+    <ul>
+      <li v-for="note in notes" :key="note.id" @click="showModal(note.id)">{{ note.title }}</li>
+    </ul>
+    <note-detail-modal
+      v-if="displayModal"
+      :note="selectedNote"
+      @close-modal-event="hideModal" />
+  </div>
+</template>
+
+<script>
+  import NoteDetailModal from '@/components/NoteDetailModal'
+
+  export default {
+    name: 'NoteList',
+    components: {
+      NoteDetailModal
+    },
+    data() {
+      return {
+        displayModal: false,
+        selectedNoteId: 0,
+        notes: [
+          {
+            id: 1,
+            title: "1st Note",
+            body: "Body of note 1",
+            date: "10/17/20",
+            isSelected: true,
+          },
+          {
+            id: 2,
+            title: "2nd Note",
+            body: "Body of note 2",
+            date: "11/17/20",
+            isSelected: false,
+          },
+          {
+            id: 3,
+            title: "3rd Note",
+            body: "Body of note 3",
+            date: "12/17/20",
+            isSelected: false,
+          }
+        ]
+      }
+    },
+    computed: {
+      selectedNote() {
+        return this.notes.filter( note => {
+          return note.id === this.selectedNoteId;
+        })[0];
+      }
+    },
+    methods: {
+      showModal(id) {
+        //console.log('showModal for id: ' + id);
+        this.selectedNoteId = id;
+        this.displayModal = true;
+      },
+      hideModal() {
+        this.displayModal = false;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
